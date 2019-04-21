8bba395bd28dc5f7b13be358e97646b1840155fd
shaga <shohaga@gmail.com>
2019-04-21 09:30:08 +0900

    update

--- app/src/main/java/com/example/dyntogglebuttontest/MainActivity.kt
+++ app/src/main/java/com/example/dyntogglebuttontest/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.dyntogglebuttontest
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.StateListDrawable
@@ -7,6 +9,7 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.RequiresApi
+import android.support.v4.content.res.ColorStateListInflaterCompat
 import android.support.v4.content.res.ResourcesCompat
 import android.util.Xml
 import android.view.View
@@ -16,6 +19,8 @@ import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.ToggleButton
 import com.google.android.flexbox.FlexboxLayout
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +28,8 @@ class MainActivity : AppCompatActivity() {
     private var add_button: Button? = null
     private var del_button: Button? = null
     private var flex_layout: FlexboxLayout? = null
-    private var sample_toggle: ToggleButton? = null
+
+    private var toggle_items: MutableList<ToggleButton> = mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +41,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun initChildViews() {
         flex_layout = findViewById(R.id.flexbox_layout)
-        sample_toggle = findViewById(R.id.toggle_sample)
         add_edit = findViewById(R.id.edit_add)
         add_button = findViewById(R.id.button_add)
         add_button?.setOnClickListener(View.OnClickListener {
@@ -44,11 +49,27 @@ class MainActivity : AppCompatActivity() {
             if (text.length == 0) return@OnClickListener
 
             var toggle = createChildToggleButton(text)
+            toggle_items.add(toggle)
+
             flex_layout?.addView(toggle)
+            add_edit?.text?.clear()
         })
         del_button = findViewById(R.id.button_del)
+        del_button?.setOnClickListener(View.OnClickListener {
+            var delete_items: MutableList<ToggleButton> = mutableListOf()
+            for (toggle_item in toggle_items) {
+                if (!toggle_item.isChecked) continue
+                delete_items.add(toggle_item)
+            }
+
+            for (toggle_item in delete_items) {
+                flex_layout?.removeView(toggle_item)
+                toggle_items.remove(toggle_item)
+            }
+        })
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun createChildToggleButton(text: String) : ToggleButton {
         var toggle = ToggleButton(this)
 
@@ -67,7 +88,20 @@ class MainActivity : AppCompatActivity() {
 
         toggle.setPadding(10.dp, 0, 10.dp, 0)
         toggle.background = ResourcesCompat.getDrawable(resources, R.drawable.dyn_toggle_selector, null)
-        toggle.setTextColor(ResourcesCompat.getColor(resources, R.color.dyn_toggle_text_color, null))
+
+        var states = arrayOf(
+            intArrayOf(android.R.attr.state_checked),
+            intArrayOf()
+        )
+
+        var colors = intArrayOf(
+            Color.WHITE,
+            Color.BLUE
+        )
+
+        var list = ColorStateList(states, colors)
+
+        toggle.setTextColor(list)
         toggle.minimumWidth = 0
         toggle.minWidth = 0
 0
